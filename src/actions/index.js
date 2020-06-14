6d29f9a9c88a298364b963dd83e2d3cd9223f1b2
Valchee <matkowski2@gmail.com>
2020-06-14 19:26:06 +0200

    Fix users overfetching, by creating new action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,19 @@
+import _ from 'lodash';
 import jsonPlaceholder from "../api/jsonPlaceholder";
+
+export const fetchPostsAndUser = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // Different version of the code below, using lodash .chain
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+}
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
@@ -11,4 +26,3 @@ export const fetchUser = id => async dispatch => {
 
   dispatch({ type: 'FETCH_USER', payload: response.data })
 }
-
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { fetchUser } from "../actions";
 import { connect } from "react-redux";
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
 
@@ -28,5 +23,4 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchUser }
 )(UserHeader);
